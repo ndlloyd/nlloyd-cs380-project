9e7ee8237c5f5cf393a1fb36e3ba87ae3b3faaef
Nathan Lloyd <nlloyd@smumn.edu>
2013-04-24 23:54:20 -0500

    Set belongs and has

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,3 +1,4 @@
 class Contest < ActiveRecord::Base
   attr_accessible :bracket, :firstContestant, :firstScore, :secondContestant, :secondScore, :tournament_id
+  belongs_to :tournaments
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,5 @@
 class Tournament < ActiveRecord::Base
   attr_accessible :fullDescription, :owner, :shortDescription, :winnerHigh
+  belongs_to :owner, :class_name => "User", :foreign_key => "user_id"
+  has_many :contests, dependent: :destroy
 end
